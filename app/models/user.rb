51a897c4e04c281f110c5f45c54db5eb1d0040e8
ichanicha <miki0426.57@gmail.com>
2024-08-04 15:51:53 +0900

    #4ユーザー管理機能（単体テスト）

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   validates  :family_name, :name,  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates  :family_name_kana, :name_kana,  format:{ with: /\A[ァ-ヶー－]+\z/ }
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+
+  validates :password, presence: true, length: { minimum: 6 }
 end
 
 
