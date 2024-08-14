3da04d716f5d60538ae4d742bbbba406a7029278
ichanicha <miki0426.57@gmail.com>
2024-08-14 17:14:39 +0900

    #8商品情報編集機能③

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  validates :name, :description, :category_id, :condition_id, :shopping_charge_id, :prefecture_id, :daily_id, :price, presence: true
+  validates :category_id, :condition_id, :shopping_charge_id, :prefecture_id, :daily_id, presence: true
   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
   belongs_to :user 
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   belongs_to :daily
   
   #空の投稿を保存できないようにする
-  validates :image, :name, :description, :category_id, :condition_id, :shopping_charge_id, :prefecture_id, :daily_id, :price, presence: true
+  validates :image, :name, :description, :category_id, :condition_id, :shopping_charge_id, :prefecture_id, :daily_id, presence: true
 
   #ジャンルの選択が「---」の時は保存できないようにする
   validates :category_id, :condition_id, :prefecture_id, :shopping_charge_id, :daily_id, numericality: { other_than: 1, message: "can't be blank" } 
